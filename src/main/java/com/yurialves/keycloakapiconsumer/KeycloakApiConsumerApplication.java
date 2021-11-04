774b9f67d6a48d9d5818b793bbbe82098bba75b9
Yuri Alves <yurileader@gmail.com>
2021-11-04 00:00:52 -0300

    Primeiras configurações

---
+++ src/main/java/com/yurialves/keycloakapiconsumer/KeycloakApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.yurialves.keycloakapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakApiConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakApiConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurialves/keycloakapiconsumer/KeycloakApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurialves.keycloakapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
