22e8388ccaaaafff2aa05dafc233486929e749ce
Yuri Oliveira <yurileader@gmail.com>
2021-11-04 09:25:09 -0300

    Atualizando endpoint para receber um objeto Mono

--- src/main/java/com/yurialves/keycloakapiconsumer/controller/ClientesExternalController.java
+++ src/main/java/com/yurialves/keycloakapiconsumer/controller/ClientesExternalController.java
@@ -1,10 +1,13 @@
 package com.yurialves.keycloakapiconsumer.controller;
 
 import com.yurialves.keycloakapiconsumer.dto.ClientesExternalDTO;
+import com.yurialves.keycloakapiconsumer.service.ClientesExternalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -16,7 +19,7 @@ public class ClientesExternalController {
     private final ClientesExternalService clientesExternalService;
 
     @GetMapping
-    List<ClientesExternalDTO> buscarClientesExternos (){
+    Mono<ResponseEntity> buscarClientesExternos (){
         return  clientesExternalService.carregarTodosClientes();
     }
 
